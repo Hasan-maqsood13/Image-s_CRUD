c58ede50abfa9918c043abf4905a4c855b6ba005
Hasan-maqsood13 <hasanmaqsood13@gmail.com>
2025-09-09 13:19:55 +0500

    initial commit: Completed the basic setup of the Project

---
+++ crud/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
+from django.contrib.auth.hashers import make_password, check_password
+from django.http import JsonResponse, HttpResponseForbidden
+from django.views.decorators.cache import never_cache
+from django.views.decorators.csrf import csrf_protect
+from ecommerce_store.settings import EMAIL_HOST_USER
+from django.views.decorators.csrf import csrf_exempt
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
+from django.utils.dateparse import parse_datetime
+from django.forms.models import model_to_dict
+from django.utils.timezone import make_aware
+from django.core.mail import send_mail
+from django.utils.text import slugify
+from django.core import serializers
+from django.contrib import messages
+from django.utils import timezone
+from django.conf import settings
+from urllib.parse import unquote
+from django.db.models import Sum
+from django.urls import reverse
+from datetime import timedelta
+from datetime import datetime
+from .models import *
+import random
+import stripe
+import json
+import re
+
+
+# Create your views here.
